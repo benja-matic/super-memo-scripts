0638c3e8bd3e8395d1a727b2f266df54bcce772b
Maciek D <maciek@marketgeni.us>
2023-10-21 23:37:06 -0700

    Initial commit.

---
+++ answers.js
@@ -0,0 +1,39 @@
+export const answers = [
+    {
+        name: "Bollard_639",
+        country: "Lesotho"
+    },
+    {
+        name: "Bollard_641",
+        country: "Japan"
+    },
+    {
+        name: "Bollard_661",
+        country: "Austria"
+    },
+    {
+        name: "Bollard_632",
+        country: "United States - California"
+    },
+    {
+        name: "Bollard_631",
+        country: "United States - Nevada"
+    },
+    {
+        name: "Bollard_613",
+        country: "Austria"
+    },
+    {
+        name: "Bollard_529",
+        country: "Austria"
+    },
+    {
+        name: "Bollard_1",
+        country: "Austria"
+    },
+    {
+        name: "Bollard_101",
+        country: "Andorra"
+    },
+
+];
---
+++ geohintsQA.js
@@ -0,0 +1,14 @@
+import * as jsdom from jsdom;
+// const { JSDOM } = jsdom;
+
+// const BASE_URL = "https://www.geohints.com/";
+// const URL_ROUTE = "Poles";
+
+// const run = async () => {
+//     const dom = new JSDOM(``, {
+//         url: `${BASE_URL}${URL_ROUTE}`,
+//     });
+//     console.log(dom);
+// };
+
+// run().then();
